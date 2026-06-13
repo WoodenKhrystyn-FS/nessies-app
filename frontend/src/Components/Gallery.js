9e26d6ada36cce9ae5089f2536440fe0042d1197
WoodenKhrystyn-FS <kvwooden@student.fullsail.edu>
2026-06-13 17:04:49 -0400

    Reveiw image assests, img descriptions

--- frontend/src/Components/Gallery.js
+++ frontend/src/Components/Gallery.js
@@ -12,18 +12,22 @@ const images = [
   {
     original: brownies,
     thumbnail: brownies,
+    description: "Chocolate Brownies",
   },
   {
     original: redVelvetCupcakes,
     thumbnail: redVelvetCupcakes,
+    description: "Red Velvet Cupcakes",
   },
   {
     original: chocolateChipCookies,
     thumbnail: chocolateChipCookies,
+    description: "Chocolate Chip Cookies",
   },
   {
     original: vanillaCake,
     thumbnail: vanillaCake,
+    description: "Vanilla Cake",
   },
 ];
 
