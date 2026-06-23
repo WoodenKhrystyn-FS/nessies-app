62e0d09dca87f2626a9edb0c059ef3dd6281b36b
WoodenKhrystyn-FS <kvwooden@student.fullsail.edu>
2026-06-22 22:11:29 -0400

    Fix routing issues and improvements on navigation

--- frontend/src/Components/GoodieCard.js
+++ frontend/src/Components/GoodieCard.js
@@ -19,10 +19,13 @@ function GoodieCard({ id, name, price, image }) {
           {available ? "Available" : "Out of Stock"}
         </p>
       </div>
-      <Button text="Create goodie" onClick={() => navigate(`/goodies/${id}/create`)} />
+
+      {/* Disabling buttons until the proper functionality is implemented */}
+
+      {/* <Button text="Create goodie" onClick={() => navigate(`/goodies/${id}/create`)} />
       <Button text="Checkout Goodies" onClick={() => navigate(`${GoodieForm}`)} />
       <Button text="Add to Cart" onClick={() => navigate("/cart")} />
-      <Button text="View Details" onClick={() => navigate(`/goodies/${id}`)} />
+      <Button text="View Details" onClick={() => navigate(`/goodies/${id}`)} /> */}
     </div>
   );
 }
