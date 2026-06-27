c318a9f014fa395b6e909ad74401240bc874643d
WoodenKhrystyn-FS <kvwooden@student.fullsail.edu>
2026-06-27 12:41:41 -0400

    Fixes: Compile errs and img of owner

--- frontend/src/Components/GoodieCard.js
+++ frontend/src/Components/GoodieCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 //import image from "../IMAGES/brownies.jpeg";
 //import { useNavigate } from "react-router-dom";
 //import GoodieForm from "./GoodieForm";
